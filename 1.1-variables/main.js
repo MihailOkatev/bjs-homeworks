c7273071fca3220a1ee0f26d1ce74ecc222acd19
Astario <mokatev@yandex.ru>
2020-01-17 02:40:26 +0300

    Выполнено задание 3 к лекции 1.1

--- 1.1-variables/main.js
+++ 1.1-variables/main.js
@@ -12,4 +12,13 @@ let myName = "Михаил Окатьев";
 let message = `Привет, мир! Меня зовут ${myName}`;
 console.log(message);
 
+// Задача 3
+console.log(`Задача 3`);
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(`Ответ ${result}`);
+
+
 
