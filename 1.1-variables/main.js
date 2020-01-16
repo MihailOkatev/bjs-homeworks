6cba64c9060bac71a47310f1c9a6c227c0d654a4
Astario <mokatev@yandex.ru>
2020-01-17 02:33:14 +0300

    Выполнено задание 2 к лекции 1.1

--- 1.1-variables/main.js
+++ 1.1-variables/main.js
@@ -1,5 +1,15 @@
+//Задача 1
+console.log(`Задача 1`);
 let algebra = 5;
 let geography = 4;
 let physics = 4;
 let averageMark = (algebra + geography + physics) / 3;
 console.log(`Средняя оценка ученика = ${averageMark} балла`);
+
+// Задача 2
+console.log(`Задача 2`);
+let myName = "Михаил Окатьев";
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message);
+
+
