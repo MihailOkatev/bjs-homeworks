8d13f594a9f9caea4c27aa96abc1da7159064b4e
Astario <mokatev@yandex.ru>
2020-02-05 18:25:59 +0300

    Решены задачи к лекции Обертки для примитивов

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,8 +1,9 @@
+"use strict";
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
     let amount = window.amount.value;
-    let date = window.date.value;
+    let date = new Date(window.date.value);
 
     let result = calculateTotalMortgage(percent, contribution, amount, date);
     let span = window.mortageResult;
@@ -10,9 +11,33 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    let tempValuee;
+    let check = parseInt(percent);
+    if (isNaN (check) === true || percent === undefined) {
+        tempValuee = percent;
+        return "Поле Процентная ставка содержит недопустимое значение: " + tempValuee;
+    }
+    check = parseInt(contribution);
+    if (isNaN (check) === true || percent === undefined) {
+        tempValuee = contribution;
+            return "Поле Первоначальный взнос содержит недопустимое значение: " + tempValuee;
+    }
+    check = parseInt(amount);
+    if (isNaN (check) === true || percent === undefined) {
+        tempValuee = amount;
+            return "Поле Общая стоимость содержит недопустимое значение: " + tempValuee;
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    console.log(date.getFullYear());
+     let totalMonth;
+     totalMonth = Math.floor((date.getTime()  - new Date().getTime()) / 1000 / 3600 / 24 / 30); // получаем количество полных месяцев с текущей даты до указанного срока
+     console.log(totalMonth);
+     let s = amount - contribution;
+     let p = percent / 12/ 100;
+     let payPerMonth = s*(p + p / (Math.pow((1 + p),totalMonth) - 1));
+     let totalAmount = payPerMonth*totalMonth;
+     console.log(typeof(totalAmount));
+     return parseFloat(totalAmount.toFixed(2));
 }
 
 function sayHello() {
@@ -23,6 +48,13 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    let check = parseInt(name);
+    console.log(check);
+    if (name === undefined || name === "" || name === null) {
+     return "Привет, мир! Меня зовут Аноним";
+    } else if (isNaN(check) === false) {
+        return "В поле имя введено числовое значение."
+    } else {
+        return "Привет, мир! Меня зовут " + name;
+    }
 }
